736e4a79058565fdd1158dbfd4317dd48ec46d0f
unknown <samjoel3101@gmail.com>
2020-09-11 13:57:22 +0530

    provided setup and folder structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup 
+
+
+setup(
+    name='django_project_structurer',
+    version='0.1',
+    description='Info Generator for a Django Project',
+    url='git@github.com:Samjoel3101/Django-Project-Structure-Generator.git',
+    author='Sam Joel',
+    author_email='samjoel.pb@gmail.com',
+    license='unlicense',
+    packages=['ptolemaios'],
+    zip_safe=False
+)
